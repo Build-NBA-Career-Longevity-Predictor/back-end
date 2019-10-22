50cb18f589ff1e4df242ab84ab175d9df13ca5c9
Ahmad Guenoun <amguenoun@gmail.com>
2019-10-22 16:24:38 -0400

    added unit and integration tests for user controller and user service impl

--- nbapredictor/src/main/java/com/lambdaschool/nbapredictor/SeedData.java
+++ nbapredictor/src/main/java/com/lambdaschool/nbapredictor/SeedData.java
@@ -1,87 +1,87 @@
-package com.lambdaschool.nbapredictor;
-
-import com.github.javafaker.Faker;
-import com.github.javafaker.service.FakeValuesService;
-import com.github.javafaker.service.RandomService;
-import com.lambdaschool.nbapredictor.models.*;
-import com.lambdaschool.nbapredictor.services.PlayerService;
-import com.lambdaschool.nbapredictor.services.RoleService;
-import com.lambdaschool.nbapredictor.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
-@Transactional
-@Component
-public class SeedData implements CommandLineRunner
-{
-    @Autowired
-    RoleService roleService;
-
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    PlayerService playerService;
-
-
-    @Override
-    public void run(String[] args) throws Exception
-    {
-        Role r1 = new Role("admin");
-        Role r2 = new Role("user");
-
-        roleService.save(r1);
-        roleService.save(r2);
-
-        // admin, user
-        ArrayList<UserRoles> admins = new ArrayList<>();
-        admins.add(new UserRoles(new User(),
-                                 r1));
-        admins.add(new UserRoles(new User(),
-                                 r2));
-        User u1 = new User("admin",
-                           "admin",
-                           "admin@admin.local",
-                           admins);
-
-        userService.save(u1);
-
-//        List<SimilarPlayer> list = new ArrayList<>();
+//package com.lambdaschool.nbapredictor;
 //
-//        Player p1 = new Player("a", "name", "pos", "25", "32", "Hurst", 25, 23, "Mik", 1.5, 1.2, 1.4, 1.4, 76,list);
+//import com.github.javafaker.Faker;
+//import com.github.javafaker.service.FakeValuesService;
+//import com.github.javafaker.service.RandomService;
+//import com.lambdaschool.nbapredictor.models.*;
+//import com.lambdaschool.nbapredictor.services.PlayerService;
+//import com.lambdaschool.nbapredictor.services.RoleService;
+//import com.lambdaschool.nbapredictor.services.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Locale;
+//
+//@Transactional
+//@Component
+//public class SeedData implements CommandLineRunner
+//{
+//    @Autowired
+//    RoleService roleService;
+//
+//    @Autowired
+//    UserService userService;
+//
+//    @Autowired
+//    PlayerService playerService;
 //
 //
-//        playerService.save(p1, u1);
-
-
-        ArrayList<UserRoles> users = new ArrayList<>();
-
-        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en-US"),
-                                                                    new RandomService());
-        Faker nameFaker = new Faker(new Locale("en-US"));
-
-        for (int i = 0; i < 10; i++)
-        {
-            new User();
-            User fakeUser;
-
-            users = new ArrayList<>();
-            users.add(new UserRoles(new User(),
-                                    r2));
-            fakeUser = new User(nameFaker.name()
-                                         .username(),
-                                "password",
-                                nameFaker.internet()
-                                         .emailAddress(),
-                                users);
-
-            userService.save(fakeUser);
-        }
-    }
-}
+//    @Override
+//    public void run(String[] args) throws Exception
+//    {
+//        Role r1 = new Role("admin");
+//        Role r2 = new Role("user");
+//
+//        roleService.save(r1);
+//        roleService.save(r2);
+//
+//        // admin, user
+//        ArrayList<UserRoles> admins = new ArrayList<>();
+//        admins.add(new UserRoles(new User(),
+//                                 r1));
+//        admins.add(new UserRoles(new User(),
+//                                 r2));
+//        User u1 = new User("admin",
+//                           "admin",
+//                           "admin@admin.local",
+//                           admins);
+//
+//        userService.save(u1);
+//
+////        List<SimilarPlayer> list = new ArrayList<>();
+////
+////        Player p1 = new Player("a", "name", "pos", "25", "32", "Hurst", 25, 23, "Mik", 1.5, 1.2, 1.4, 1.4, 76,list);
+////
+////
+////        playerService.save(p1, u1);
+//
+//
+//        ArrayList<UserRoles> users = new ArrayList<>();
+//
+//        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en-US"),
+//                                                                    new RandomService());
+//        Faker nameFaker = new Faker(new Locale("en-US"));
+//
+//        for (int i = 0; i < 10; i++)
+//        {
+//            new User();
+//            User fakeUser;
+//
+//            users = new ArrayList<>();
+//            users.add(new UserRoles(new User(),
+//                                    r2));
+//            fakeUser = new User(nameFaker.name()
+//                                         .username(),
+//                                "password",
+//                                nameFaker.internet()
+//                                         .emailAddress(),
+//                                users);
+//
+//            userService.save(fakeUser);
+//        }
+//    }
+//}
---
+++ nbapredictor/src/test/java/com/lambdaschool/nbapredictor/SeedData.java
@@ -0,0 +1,83 @@
+package com.lambdaschool.nbapredictor;
+
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+import com.lambdaschool.nbapredictor.models.*;
+import com.lambdaschool.nbapredictor.services.PlayerService;
+import com.lambdaschool.nbapredictor.services.RoleService;
+import com.lambdaschool.nbapredictor.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+@Transactional
+@Component
+public class SeedData implements CommandLineRunner
+{
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    PlayerService playerService;
+
+
+    @Override
+    public void run(String[] args) throws Exception {
+        Role r1 = new Role("admin");
+        Role r2 = new Role("user");
+        Role r3 = new Role("data");
+
+        roleService.save(r1);
+        roleService.save(r2);
+        roleService.save(r3);
+
+        // admin, data, user
+        ArrayList<UserRoles> admins = new ArrayList<>();
+        admins.add(new UserRoles(new User(), r1));
+        admins.add(new UserRoles(new User(), r2));
+        admins.add(new UserRoles(new User(), r3));
+        User u1 = new User("admin", "ILuvM4th!", "admin@lambdaschool.local", admins);
+        u1 = userService.save(u1);
+
+        // data, user
+        ArrayList<UserRoles> datas = new ArrayList<>();
+        datas.add(new UserRoles(new User(), r3));
+        datas.add(new UserRoles(new User(), r2));
+        User u2 = new User("cinnamon", "1234567", "cinnamon@lambdaschool.local", datas);
+
+        u2 = userService.save(u2);
+
+        // user
+        ArrayList<UserRoles> users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r1));
+        User u3 = new User("testbarn", "ILuvM4th!", "testbarn@school.lambda", users);
+
+        u3 = userService.save(u3);
+
+        users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u4 = new User("testcat", "password", "testcat@school.lambda", users);
+        u4 = userService.save(u4);
+
+        users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u5 = new User("testdog", "password", "testdog@school.lambda", users);
+        u5 = userService.save(u5);
+
+        System.out.println("\n*** Seed Data ***");
+        System.out.println(u1);
+        System.out.println(u2);
+        System.out.println(u3);
+        System.out.println(u4);
+        System.out.println(u5);
+        System.out.println("*** Seed Data ***\n");
+    }
+}
